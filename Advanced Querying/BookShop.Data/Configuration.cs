e905f70784fa4fb962ccc0fa131e62a8ab8a9553
Valeri Hristov <valerimagic@gmail.com>
2021-03-03 11:49:26 +0200

    new

---
+++ Advanced Querying/BookShop.Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace BookShop.Data
+{
+    internal class Configuration
+    {
+        public const string ConnectionString =
+            "Server=DESKTOP-Q9B2B1D\\SQLEXPRESS;Database=BookShop;Integrated Security=true";
+    }
+}
